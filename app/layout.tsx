fd73206006fe3ca87e415d2bed7169ae44faaa9c
Simone Westphal <simone.westphal.me@gmail.com>
2025-04-30 20:06:02 +0200

    clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
 import Link from 'next/link'
-import { ChartColumn, ChartColumnBigIcon } from 'lucide-react'
+import { ChartColumn } from 'lucide-react'
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
