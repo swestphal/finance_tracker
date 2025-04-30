445fc03edf1c2a7e7bbeb12803a74a80c8374c6e
Simone Westphal <simone.westphal.me@gmail.com>
2025-04-30 20:13:54 +0200

    add basic clerk login/signup button

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,14 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import Link from 'next/link'
 import { ChartColumn } from 'lucide-react'
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from '@clerk/nextjs'
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -21,17 +29,32 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${poppins.variable} antialiased`}>
-        <nav className="bg-primary p-4 text-white h-20 flex items-center justify-between">
-          <Link href="/" className="font-bold text-2xl flex gap-1 items-center">
-            <ChartColumn className="text-lime-500" />
-            FinanceTracker
-          </Link>
-          <div>auth button</div>
-        </nav>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${poppins.variable} antialiased`}>
+          <nav className="bg-primary p-4 text-white h-20 flex items-center justify-between">
+            <Link
+              href="/"
+              className="font-bold text-2xl flex gap-1 items-center"
+            >
+              <ChartColumn className="text-lime-500" />
+              FinanceTracker
+            </Link>
+            <div>
+              <SignedOut>
+                <div className="flex items-center">
+                  <SignInButton />
+                  <SignUpButton />
+                </div>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </nav>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
