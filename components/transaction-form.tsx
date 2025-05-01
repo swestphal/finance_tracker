ff1e656485b929ddf5f1e7d4e3ae3863c8f815c9
Simone Westphal <simone.westphal.me@gmail.com>
2025-05-01 11:44:01 +0200

    add inputs to transaction form

--- components/transaction-form.tsx
+++ components/transaction-form.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { addDays } from 'date-fns'
+import { addDays, format, setDate } from 'date-fns'
 import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+import { date, z } from 'zod'
 import {
   Form,
   FormControl,
@@ -19,6 +19,13 @@ import {
   SelectTrigger,
   SelectValue,
 } from './ui/select'
+
+import { cn } from '@/lib/utils'
+import { CalendarIcon } from 'lucide-react'
+import { Button } from './ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+import { Calendar } from './ui/calendar'
+import { Input } from './ui/input'
 
 const transactionFormSchema = z.object({
   transactionType: z.enum(['income', 'expense']),
@@ -44,7 +51,10 @@ export default function TransactionForm() {
       transactionType: 'income',
     },
   })
-  const handleSubmit = async (data: z.infer<typeof transactionFormSchema>) => {}
+
+  const handleSubmit = async (data: z.infer<typeof transactionFormSchema>) => {
+    console.log(data)
+  }
 
   return (
     <Form {...form}>
@@ -67,6 +77,22 @@ export default function TransactionForm() {
                         <SelectItem value="expense">Expense</SelectItem>
                       </SelectContent>
                     </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+
+          <FormField
+            control={form.control}
+            name="amount"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="number" />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -100,6 +126,66 @@ export default function TransactionForm() {
               )
             }}
           />
+
+          <FormField
+            control={form.control}
+            name="transactionDate"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Transaction Date</FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={'outline'}
+                          className={cn(
+                            'justify-start text-left font-normal',
+                            !field.value && 'text-muted-foreground'
+                          )}
+                        >
+                          <CalendarIcon className="mr-2 h-4 w-4" />
+                          {field.value ? (
+                            format(field.value, 'PPP')
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          initialFocus
+                          disabled={{ after: new Date() }}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+        </fieldset>
+        <fieldset className="mt-5 flex flex-col gap-5">
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+          <Button type="submit">Submit</Button>
         </fieldset>
       </form>
     </Form>
