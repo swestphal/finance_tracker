b8ed136c13186695099f6868804c8711d32d255b
Simone Westphal <simone.westphal.me@gmail.com>
2025-05-03 18:48:17 +0200

    style toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,12 +61,15 @@ export default function RootLayout({
           </nav>
           {children}
           <Toaster
+            className="toaster group"
             position="bottom-center"
             toastOptions={{
               classNames: {
-                toast: 'toast',
-                title: 'text-lg font-semibold',
+                toast:
+                  'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+                title: '!text-lg font-semibold',
                 description: '!text-black',
+                icon: 'group-data-[type=error]:text-red-500 group-data-[type=success]:text-green-500 group-data-[type=warning]:text-amber-500 group-data-[type=info]:text-blue-500',
               },
             }}
           />
