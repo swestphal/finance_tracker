54d569dea955bc187f5d84c1d9de2fb7abc37f20
Simone Westphal <simone.westphal.me@gmail.com>
2025-05-04 11:29:47 +0200

    add searchParams

--- app/dashboard/transactions/page.tsx
+++ app/dashboard/transactions/page.tsx
@@ -1,15 +1,69 @@
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from '@/components/ui/breadcrumb'
+import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { format } from 'date-fns'
 import Link from 'next/link'
+import { z } from 'zod'
 
-export default function DashboardPage() {
+const today = new Date()
+
+const searchSchema = z
+  .object({
+    year: z.coerce
+      .number()
+      .min(today.getFullYear() - 100)
+      .max(today.getFullYear() + 1)
+      .catch(today.getFullYear()),
+    month: z.coerce
+      .number()
+      .min(1)
+      .max(12)
+      .catch(today.getMonth() + 1),
+  })
+  .strict()
+export default async function TransactionsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ year?: string; month?: string }>
+}) {
+  const searchParamsValues = await searchParams
+  const { month, year } = searchSchema.parse(searchParamsValues)
+  const selectedDate = new Date(year, month - 1, 1)
   return (
-    <Card className="mt-4 max-w-screen-md">
-      <CardHeader>
-        <CardTitle>Transactions</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Link href="/dashboard/transactions/new">New Transaction</Link>
-      </CardContent>
-    </Card>
+    <div className="max-w-screen-xl mx-auto py-10">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/dashboard">Dashboard</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Transactions</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <Card className="mt-4">
+        <CardHeader>
+          <CardTitle className="flex justify-between">
+            <span>{format(selectedDate, 'MMM yyyy')} Transactions</span>
+            <div>dropdowns</div>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Button asChild>
+            <Link href="/dashboard/transactions/new">New Transaction</Link>
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
